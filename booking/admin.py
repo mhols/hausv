3db1b22d48effc97f397dd5d5a2deeebad14c74d
Prof. Dr. Matthias Holschneider <matthias.holschneider@uni-potsdam.de>
2018-10-18 15:17:22 +0200

    go for it

--- booking/admin.py
+++ booking/admin.py
@@ -2,3 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
+from .models import Konto, Buchung, Bilanz
+
+admin.site.register(Konto)
+admin.site.register(Buchung)
+admin.site.register(Bilanz)

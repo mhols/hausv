ad8acefef30e522546c5fd7e27b6c3c338799838
Prof. Dr. Matthias Holschneider <hols@uni-potsdam.de>
2018-10-25 16:44:31 +0200

    progressing

--- booking/views.py
+++ booking/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+
 import django_tables2 as tables
 
 from .models import Konto
-
 
 
 def date_to_str(da):
@@ -15,6 +16,36 @@ def date_to_str(da):
     
     return "%s-%s-%s"%(da.year, pretty(da.month), pretty(da.day))
 
+
+class KontenView(ListView):
+    
+    model = Konto
+    
+    class KontenTable(tables.Table):
+        kurz = tables.Column()
+        lang  = tables.Column()
+        art   = tables.Column()
+        soll  = tables.Column()
+        haben = tables.Column()
+        delta = tables.Column()
+        url   = tables.Column()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(DetailView, self).get_context_data(*args, **kwargs)
+    
+        data = {}
+        for k in self.object_list:
+            data.update(
+            {
+                'kurz' : k.kurz,
+                'lang' : k.lang,
+                'soll' : sum ([b.wert for b in k.soll_buchungen.all() ])
+            }    
+            )
+    
+        return context
+    
+    
 # Create your views here.
 class KontoView(DetailView):
     
@@ -46,6 +77,7 @@ class KontoView(DetailView):
         print (self.request)
         knt = self.object
         
+        #print ('last saldo ', knt.last_saldo())
         sb = knt.soll_buchungen.all()
         hb = knt.haben_buchungen.all()
         
@@ -77,7 +109,33 @@ class KontoView(DetailView):
                             'erkl' : b.beschreibung,
                             'gegenk' : b.sollkonto
                             }
-                    )
+                        )
+        soll, haben = knt.saldiere()
+        
+        data.append({'datum' : date_to_str(d),
+                     'soll'  : "%10.2f"%(soll/100),
+                     'erkl' : 'SUMME soll',
+                     'gegenk' : knt})
+        
+        data.append({'datum' : date_to_str(d),
+                     'haben'  : "%10.2f"%(haben/100),
+                     'erkl' : 'SUMME haben',
+                     'gegenk' : knt})
+        
+        if soll >= haben:
+            data.append({'datum' : date_to_str(d),
+                     'haben'  : "%10.2f"%((soll-haben)/100),
+                     'erkl' : 'SALDO',
+                     'gegenk' : knt})
+        else:
+            data.append({'datum' : date_to_str(d),
+                     'soll'  : "%10.2f"%(haben/100),
+                     'erkl' : 'SALDO',
+                     'gegenk' : knt})
+        
+        
+        
+        
         tab = KontoView.KontoTable( data )
         context.update( {'tab' : tab, 
                          'konto' : knt.lang } )
--- scripts/maintenance.py
+++ scripts/maintenance.py
@@ -23,38 +23,39 @@ def run():
     # Kontenplan
     
     Kontenplan = [
-        Konto(art=Konto.aktiv_bestand,  kurz='B',         lang='Deutsche Bank Konto'), 
-        Konto(art=Konto.aktiv_bestand,  kurz='F',         lang='Forderungen'),
-        Konto(art=Konto.aktiv_bestand,  kurz='F.Holger',  lang='Mieter Konto Holger'),
-        Konto(art=Konto.aktiv_bestand,  kurz='F.Roeder',  lang='Mieter Konto Roeder'),
-        Konto(art=Konto.aktiv_bestand,  kurz='F.Peter',   lang='Mieter Konto Peter'),
-        Konto(art=Konto.passiv_eigenkmain, kurz='EK',        lang='Gesellschafter Kapital'),
-        Konto(art=Konto.passiv_eigenk, kurz='EK.Peter',  lang='Gesellschafter Konto Peter'),
-        Konto(art=Konto.passiv_eigenk, kurz='EK.Alex',   lang='Gesellschafter Konto Alex'),
-        Konto(art=Konto.passiv_bestand, kurz='V',         lang='Verbindlichkeiten'),
-        Konto(art=Konto.passiv_bestand, kurz='V.H',       lang='Verpflichtungen Handwerker'),
-        Konto(art=Konto.passiv_bestand, kurz='V.H.Kadel', lang='Verbindl Handwerker Kadel'),
-        Konto(art=Konto.passiv_ertrag,  kurz='ER',        lang='Allgemeines Gegenkonto fuer Ertraege'),
-        Konto(art=Konto.passiv_aufwand, kurz='RE',        lang='Instandhaltung, Reparaturen'),
-        Konto(art=Konto.passiv_aufwand, kurz='RE.UG',     lang='Instandhaltung, Reparaturen UG'),
-        Konto(art=Konto.passiv_aufwand, kurz='RE.1OG',    lang='Instandhaltung, Reparaturen 1OG'),
-        Konto(art=Konto.passiv_aufwand, kurz='RE.2OG',    lang='Instandhaltung, Reparaturen 2OG'),
-        Konto(art=Konto.passiv_aufwand, kurz='VERW',      lang='Verwaltungskosten'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK',        lang='Nebenkosten'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.GS',     lang='Grundsteuer'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.VS',     lang='Versicherungen'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.ML',     lang='Hausmuell'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.KW',     lang='Kaltwasser'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.HZ',     lang='Heizung'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.HZ.FL',  lang='Feuerloescher'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.HZ.GAS', lang='Gasverbrauch'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.HZ.WART',lang='Wartung HZ'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.HZ.KAMIN',lang='Kaminfeger'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.HZ.STR', lang='Heizstom'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.HZ.ABR', lang='Abrechnungsservice'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.STR',    lang='Hausstrom'),        
-        Konto(art=Konto.passiv_aufwand, kurz='NK.GARTEN', lang='Gartenarbeiten'),
-        Konto(art=Konto.passiv_aufwand, kurz='NK.TECHEM', lang='Abrechnung NK'),
+        Konto.create(art=Konto.bilanzkonto,  kurz='BILANZ',    lang='Bilanzkonto'), 
+        Konto.create(art=Konto.aktiv_bestand,  kurz='B',         lang='Deutsche Bank Konto'), 
+        Konto.create(art=Konto.aktiv_bestand,  kurz='F',         lang='Forderungen'),
+        Konto.create(art=Konto.aktiv_bestand,  kurz='F.Holger',  lang='NK Forderungen Holger'),
+        Konto.create(art=Konto.aktiv_bestand,  kurz='F.Roeder',  lang='NK Forderungen Roeder'),
+        Konto.create(art=Konto.aktiv_bestand,  kurz='F.Peter',   lang='NK Forderungen Peter'),
+        Konto.create(art=Konto.passiv_eigenkmain, kurz='EK',     lang='Gesellschafter Kapital'),
+        Konto.create(art=Konto.passiv_eigenk, kurz='EK.Peter',   lang='Gesellschafter Konto Peter'),
+        Konto.create(art=Konto.passiv_eigenk, kurz='EK.Alex',    lang='Gesellschafter Konto Alex'),
+        Konto.create(art=Konto.passiv_bestand, kurz='V',         lang='Verbindlichkeiten'),
+        Konto.create(art=Konto.passiv_bestand, kurz='V.H',       lang='Verpflichtungen Handwerker'),
+        Konto.create(art=Konto.passiv_bestand, kurz='V.H.Kadel', lang='Verbindl Handwerker Kadel'),
+        Konto.create(art=Konto.passiv_ertrag,  kurz='ER',        lang='Allgemeines Gegenkonto fuer Ertraege'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='RE',        lang='Instandhaltung, Reparaturen'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='RE.UG',     lang='Instandhaltung, Reparaturen UG'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='RE.1OG',    lang='Instandhaltung, Reparaturen 1OG'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='RE.2OG',    lang='Instandhaltung, Reparaturen 2OG'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='VERW',      lang='Verwaltungskosten'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK',        lang='Nebenkosten'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.GS',     lang='Grundsteuer'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.VS',     lang='Versicherungen'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.ML',     lang='Hausmuell'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.KW',     lang='Kaltwasser'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.HZ',     lang='Heizung'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.HZ.FL',  lang='Feuerloescher'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.HZ.GAS', lang='Gasverbrauch'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.HZ.WART',lang='Wartung HZ'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.HZ.KAMIN',lang='Kaminfeger'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.HZ.STR', lang='Heizstom'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.HZ.ABR', lang='Abrechnungsservice'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.STR',    lang='Hausstrom'),        
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.GARTEN', lang='Gartenarbeiten'),
+        Konto.create(art=Konto.passiv_aufwand, kurz='NK.TECHEM', lang='Abrechnung NK'),
     ]
     for K in Kontenplan:
         try:
@@ -67,21 +68,26 @@ def run():
     
     Anfangssalden = []
     for K in Kontenplan:
-        if K.kurz in Anfangsbilanz:
-            S = Saldo.create_unsigned(K, Anfangsbilanz[K.kurz])
-        else:
-            S = Saldo.create_unsigned(K, 0)
-        S.save()
-        Anfangssalden.append(S)
+        if not (K.art == Konto.bilanzkonto):
+            if K.kurz in Anfangsbilanz:
+                S = Saldo.create_unsigned(K, Anfangsbilanz[K.kurz])
+            else:
+                S = Saldo.create_unsigned(K, 0)
+            S.save()
+            Anfangssalden.append(S)
     
     Banfang = Bilanz.create( date(2018,1,1), 'H22-2018', Anfangssalden )
+    
+    
     
     print (Banfang)
     
     
     
     Buchungen = [
-" 1.10.2018 : NK.ML : EK.Peter : 13400 : Muell Gebuehren gezahlt von Peter: None ",        
+" 30.1.2018 : B : BILANZ : 558585 : Anfangsbilanz: None ",        
+" 30.1.2018 : BILANZ : EK : 558585 : Anfangsbilanz: None ",        
+" 1.1.2018 : NK.ML : EK.Peter : 13400 : Muell Gebuehren gezahlt von Peter: None ",        
 " 1.10.2018 : B : F.Peter : 22000 : NK Voraus Peter: None ",
 " 1.10.2018 : B : F.Roeder : 14000 : NK Voraus Roeder: None ",
 " 1.10.2018 : NK.HZ.GAS : B : 14200 : Stadtwerke Waldkirch Gas: None ",
@@ -173,39 +179,41 @@ def run():
 " 5.2.2018 : B : F.Holger : 20000 : Vorauszahlung NK Holger: None ",
 " 5.3.2018 : B : F.Holger : 20000 : Vorauszahlung NK Holger: None ",
 " 6.8.2018 : B : F.Holger : 20000 : Vorauszahlung NK Holger: None ",
-"1.2.2018 : F.Holger : EK : 20000 : Nebenkosten Pauschale Holger : None",
-"1.2.2018 : F.Peter : EK : 22000 : Nebenkosten Pauschale Peter : None",
-"1.2.2018 : F.Roeder : EK : 14000 : Nebenkosten Pauschale Roeder : None",
-"1.3.2018 : F.Holger : EK : 20000 : Nebenkosten Pauschale Holger : None",
-"1.3.2018 : F.Peter : EK : 22000 : Nebenkosten Pauschale Peter : None",
-"1.3.2018 : F.Roeder : EK : 14000 : Nebenkosten Pauschale Roeder : None",
-"1.4.2018 : F.Holger : EK : 20000 : Nebenkosten Pauschale Holger : None",
-"1.4.2018 : F.Peter : EK : 22000 : Nebenkosten Pauschale Peter : None",
-"1.4.2018 : F.Roeder : EK : 14000 : Nebenkosten Pauschale Roeder : None",
-"1.5.2018 : F.Holger : EK : 20000 : Nebenkosten Pauschale Holger : None",
-"1.5.2018 : F.Peter : EK : 22000 : Nebenkosten Pauschale Peter : None",
-"1.5.2018 : F.Roeder : EK : 14000 : Nebenkosten Pauschale Roeder : None",
-"1.6.2018 : F.Holger : EK : 20000 : Nebenkosten Pauschale Holger : None",
-"1.6.2018 : F.Peter : EK : 22000 : Nebenkosten Pauschale Peter : None",
-"1.6.2018 : F.Roeder : EK : 14000 : Nebenkosten Pauschale Roeder : None",
-"1.7.2018 : F.Holger : EK : 20000 : Nebenkosten Pauschale Holger : None",
-"1.7.2018 : F.Peter : EK : 22000 : Nebenkosten Pauschale Peter : None",
-"1.7.2018 : F.Roeder : EK : 14000 : Nebenkosten Pauschale Roeder : None",
-"1.8.2018 : F.Holger : EK : 20000 : Nebenkosten Pauschale Holger : None",
-"1.8.2018 : F.Peter : EK : 22000 : Nebenkosten Pauschale Peter : None",
-"1.8.2018 : F.Roeder : EK : 14000 : Nebenkosten Pauschale Roeder : None",
-"1.9.2018 : F.Holger : EK : 20000 : Nebenkosten Pauschale Holger : None",
-"1.9.2018 : F.Peter : EK : 22000 : Nebenkosten Pauschale Peter : None",
-"1.9.2018 : F.Roeder : EK : 14000 : Nebenkosten Pauschale Roeder : None",
-"1.10.2018 : F.Holger : EK : 20000 : Nebenkosten Pauschale Holger : None",
-"1.10.2018 : F.Peter : EK : 22000 : Nebenkosten Pauschale Peter : None",
-"1.10.2018 : F.Roeder : EK : 14000 : Nebenkosten Pauschale Roeder : None",
+"1.1.2018 : F.Holger : ER : 20000 : Nebenkosten Pauschale Holger : None",
+"1.1.2018 : F.Peter : ER : 22000 : Nebenkosten Pauschale Peter : None",
+"1.1.2018 : F.Roeder : ER : 14000 : Nebenkosten Pauschale Roeder : None",
+"1.2.2018 : F.Holger : ER : 20000 : Nebenkosten Pauschale Holger : None",
+"1.2.2018 : F.Peter : ER : 22000 : Nebenkosten Pauschale Peter : None",
+"1.2.2018 : F.Roeder : ER : 14000 : Nebenkosten Pauschale Roeder : None",
+"1.3.2018 : F.Holger : ER : 20000 : Nebenkosten Pauschale Holger : None",
+"1.3.2018 : F.Peter : ER : 22000 : Nebenkosten Pauschale Peter : None",
+"1.3.2018 : F.Roeder : ER : 14000 : Nebenkosten Pauschale Roeder : None",
+"1.4.2018 : F.Holger : ER : 20000 : Nebenkosten Pauschale Holger : None",
+"1.4.2018 : F.Peter : ER : 22000 : Nebenkosten Pauschale Peter : None",
+"1.4.2018 : F.Roeder : ER : 14000 : Nebenkosten Pauschale Roeder : None",
+"1.5.2018 : F.Holger : ER : 20000 : Nebenkosten Pauschale Holger : None",
+"1.5.2018 : F.Peter : ER : 22000 : Nebenkosten Pauschale Peter : None",
+"1.5.2018 : F.Roeder : ER : 14000 : Nebenkosten Pauschale Roeder : None",
+"1.6.2018 : F.Holger : ER : 20000 : Nebenkosten Pauschale Holger : None",
+"1.6.2018 : F.Peter : ER : 22000 : Nebenkosten Pauschale Peter : None",
+"1.6.2018 : F.Roeder : ER : 14000 : Nebenkosten Pauschale Roeder : None",
+"1.7.2018 : F.Holger : ER : 20000 : Nebenkosten Pauschale Holger : None",
+"1.7.2018 : F.Peter : ER : 22000 : Nebenkosten Pauschale Peter : None",
+"1.7.2018 : F.Roeder : ER : 14000 : Nebenkosten Pauschale Roeder : None",
+"1.8.2018 : F.Holger : ER : 20000 : Nebenkosten Pauschale Holger : None",
+"1.8.2018 : F.Peter : ER : 22000 : Nebenkosten Pauschale Peter : None",
+"1.8.2018 : F.Roeder : ER : 14000 : Nebenkosten Pauschale Roeder : None",
+"1.9.2018 : F.Holger : ER : 20000 : Nebenkosten Pauschale Holger : None",
+"1.9.2018 : F.Peter : ER : 22000 : Nebenkosten Pauschale Peter : None",
+"1.9.2018 : F.Roeder : ER : 14000 : Nebenkosten Pauschale Roeder : None",
 ]
 
     for b in Buchungen:
         generate_buchung(b)
         
     Bende = make_bilanz(Banfang, Buchung.objects.all(), date(2018,12,31), 'H22-2019')
+
+
     
     print (Bende)
     

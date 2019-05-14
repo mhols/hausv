2e5f107120ef44b815ff2e80280a7fe3be8c0c4d
Prof. Dr. Matthias Holschneider <hols@uni-potsdam.de>
2019-05-14 07:32:20 +0200

    progressing

--- manip.py
+++ manip.py
@@ -11,9 +11,49 @@ from goodies.util import *
 
 from datetime import date
 
-for b in Buchung.objects.filter(datum__lt=date(2018,1,1)).all():
-	b.delete()
-
-	
-
-
+for y in [2018, 2019]:
+	for m in range(1,13):
+		dd = date( y, m, 1)
+		for w, k in zip([22000, 20000, 14000], [ 'Peter', 'Holger', 'Roeder']):
+			kk = Konto.objects.get(kurz='H22.F.'+k)
+			knk = Konto.objects.get(kurz='H22.EK.ER.NK')
+			
+			b = Buchung()
+			b.datum = dd
+			b.sollkonto = kk
+			b.habenkonto = knk
+			b.wert = w
+			b.beschreibung = 'Nebenkosten Pauschale '+ k
+			try:
+				b.save()
+			except:
+				pass
+			
+		for w, k in zip([14000, 7000], [ 'Peter', 'Alex']):
+			kk = Konto.objects.get(kurz='H22.F.'+k)
+			knk = Konto.objects.get(kurz='H22.EK.'+k)
+			
+			b = Buchung()
+			b.datum = dd
+			b.sollkonto = kk
+			b.habenkonto = knk
+			b.wert = w
+			b.beschreibung = 'Hausgeld Pauschale '+ k
+			try:
+				b.save()
+			except:
+				pass
+		for w, k in zip([4000,], [ 'Peter',]):
+			kk = Konto.objects.get(kurz='H22.F.'+k)
+			knk = Konto.objects.get(kurz='H22.EK.A.VERW')
+			
+			b = Buchung()
+			b.datum = dd
+			b.sollkonto = knk
+			b.habenkonto = kk
+			b.wert = w
+			b.beschreibung = 'Verwaltungs Pauschale '+ k
+			try:
+				b.save()
+			except:
+				pass
